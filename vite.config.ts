3a50313341575356a388fb8393e41e8aeab3f8cc
Laurensvc <laurens.van.c@gmail.com>
2026-02-07 22:12:42 +0100

    netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
 
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   resolve: {
@@ -16,7 +16,7 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    nitro(),
+    netlify(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
